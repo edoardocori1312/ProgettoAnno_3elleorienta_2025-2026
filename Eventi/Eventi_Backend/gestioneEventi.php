a99257a96976bd9ae3d0aef2b2d76bd6bbdbea3e
manuel-iossa <st10875@iismarconipieralisi.it>
2026-04-21 19:59:50 +0200

    Versione Aggiornata Di Gestione Eventi

--- Eventi/Eventi_Backend/gestioneEventi.php
+++ Eventi/Eventi_Backend/gestioneEventi.php
@@ -26,19 +26,12 @@
     }
 
 
-    function bindParams($stmt, $types, array &$values) {
-        $params = [];
-    
-        // primo parametro: tipi
-        $params[] = &$types;
-
-        // aggiungo i valori come riferimenti
-        foreach ($values as $key => $value) {
-            $params[] = &$values[$key];
-        }
-
-        call_user_func_array([$stmt, 'bind_param'], $params);
+    function bindParams($stmt, $types, array $values) {
+    if ($types != "") {
+        // L'operatore ... spacchetta l'array in argomenti singoli
+        $stmt->bind_param($types, ...$values);
     }
+}
     
     //filtri: generale, titolo, target, range date
     function filtriVisualizza($statement, $conn, $idUtente, $tipoRicerca, $titolo, $desc, $target, $data_inizio, $data_fine, $citta){
@@ -166,7 +159,7 @@
         $sql = "SELECT e.* FROM eventi e"; //query da eseguire
         $res = null;
 
-        //try{ //try per evitare che il programma crepi
+        try{ //try per evitare che il programma crepi
 
             global $HOSTDB, $USERDB, $NOMEDB, $PASSDB; 
             $conn = new mysqli ($HOSTDB, $USERDB, $PASSDB, $NOMEDB);
@@ -185,18 +178,18 @@
             if($stmt->execute() === true) { //controllo se l'esecuzione dello statement è andata a buon fine
                 $res=$stmt->get_result();
             }
-        //}
-        //catch(Throwable $e){ //catch di tutti i Throwable, include sia errori che eccezioni
-        //    global $file_connessione;
-        //    if(!file_exists($file_connessione)){ //se il file è mancante o non trovato ritorno l'errore appropriato
-        //        $risultato->errore = "Errore durante l'inclusione del file di configurazione";
-        //    }
-        //    else{
-        //        $risultato->errore = "Errore durante l'esecuzione della query";
-        //    }
+        }
+        catch(Throwable $e){ //catch di tutti i Throwable, include sia errori che eccezioni
+            global $file_connessione;
+           if(!file_exists($file_connessione)){ //se il file è mancante o non trovato ritorno l'errore appropriato
+                $risultato->errore = "Errore durante l'inclusione del file di configurazione";
+            }
+            else{
+               $risultato->errore = "Errore durante l'esecuzione della query";
+            }
             
-        //    return $risultato; //interrompo l'esecuzione della funzione
-        //}
+            return $risultato; //interrompo l'esecuzione della funzione
+        }
 
         if(controlloRisultato($res)) //se il risultato è settato
         {
@@ -241,7 +234,7 @@
             $ruoloUtente = ruoloUtente($idUtente, $conn); 
             if($ruoloUtente == "ADMIN")
             {
-                $sql = "insert into eventi (titolo, descrizione, target, ora_inizio, ora_fine, visibile, prenotabile, via_P, n_civico_P, coordinate, descrizione_breve) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"; 
+                
                 
             }   
             else if ($ruoloUtente == "SCOLASTICO")
@@ -252,6 +245,8 @@
             {
                 $risultato->errore = "errore nel trovare il ruolo dell'utente"; 
             }
+
+            $sql = "insert into eventi (titolo, descrizione, target, ora_inizio, ora_fine, visibile, prenotabile, via_P, n_civico_P, coordinate, descrizione_breve) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
             $stmt = $conn->prepare($sql);
             
             if($stmt === false)
@@ -299,34 +294,41 @@
         }
         else
         {
-            $sql = "update eventi set titolo = ?, descrizione = ?, target = ?, ora_inizio = ?, ora_fine = ?, visibile = ?, prenotabile = ?, via_P = ?, n_civico_P = ?, coordinate = ?, descrizione_breve = ? where ID_evento = ?";
-            
-            $stmt = $conn->prepare($sql); 
-
-            if($stmt === false)
+            if(!verificaPermessi($idUtente, $idEvento, $conn))
             {
-                $risultato->errore = "errore nella creazione dello statement"; 
+                $risultato->errore = "errore nei permessi"; 
             }
-            $stmt->bind_param("sssssiisissi", $titolo, $descrizione, $target, $ora_inizio, $ora_fine, $visibile, $prenotabile, $via_P, $n_civico_P, $coordinate, $descrizioneBreve, $idEvento); 
             else
             {
-                if(!$stmt->execute())
+                $sql = "update eventi set titolo = ?, descrizione = ?, target = ?, ora_inizio = ?, ora_fine = ?, visibile = ?, prenotabile = ?, via_P = ?, n_civico_P = ?, coordinate = ?, descrizione_breve = ? where ID_evento = ?";
+                
+                $stmt = $conn->prepare($sql); 
+
+                if($stmt === false)
                 {
-                    $risultato->errore = "errore nell'esecuzione dello statement";
+                    $risultato->errore = "errore nella creazione dello statement"; 
+                } 
+                else
+                {
+                    $stmt->bind_param("sssssiisissi", $titolo, $descrizione, $target, $ora_inizio, $ora_fine, $visibile, $prenotabile, $via_P, $n_civico_P, $coordinate, $descrizioneBreve, $idEvento);
+                    if(!$stmt->execute())
+                    {
+                        $risultato->errore = "errore nell'esecuzione dello statement";
+                    }
+                    else
+                    {
+                        $res = $stmt->get_result(); 
+                    }
+                }
+
+                if(controlloRisultato($res))
+                {
+                    $risultato->result = $res; 
                 }
                 else
                 {
-                    $res = $stmt->get_result(); 
+                    $risultato->errore = "La query ha ritornato un risultato non valido"; 
                 }
-            }
-
-            if(controlloRisultato($res))
-            {
-                $risultato->result = $res; 
-            }
-            else
-            {
-                $risultato->errore = "La query ha ritornato un risultato non valido"; 
             }
             $stmt->close(); 
             $conn->close(); 
@@ -343,32 +345,39 @@
         $conn = new mysqli ($HOSTDB, $USERDB, $PASSDB, $NOMEDB);
         $stmt = null;
 
-        $sql = "update eventi set data_eliminazione = ? where ID_evento = ?"; 
-
-        $stmt = $conn->prepare($sql); 
-
-        if($stmt === false)
+        if(!verificaPermessi($idUtente, $idEvento, $conn))
         {
-            $risultato->errore = "errore nella preparazione dello statement"; 
-        }
-
-        $stmt->bind_param("si", $data_eliminazione, $idEvento); 
-
-        if(!$stmt->execute())
-        {
-            $risultato->errore = "errore nell'esecuzione dello statement"; 
+            $risultato->errore = "errore nei permessi"; 
         }
         else
         {
-            $res = $stmt->get_result(); 
-        }
-        if(controlloRisultato($res))
-        {
-            $risultato->result = $res; 
-        }
-        else
-        {
-            $risultato->errore = "La query ha ritornato un risultato non valido";
+            $sql = "update eventi set data_eliminazione = ? where ID_evento = ?"; 
+
+            $stmt = $conn->prepare($sql); 
+
+            if($stmt === false)
+            {
+                $risultato->errore = "errore nella preparazione dello statement"; 
+            }
+
+            $stmt->bind_param("si", $data_eliminazione, $idEvento); 
+
+            if(!$stmt->execute())
+            {
+                $risultato->errore = "errore nell'esecuzione dello statement"; 
+            }
+            else
+            {
+                $res = $stmt->get_result(); 
+            }
+            if(controlloRisultato($res))
+            {
+                $risultato->result = $res; 
+            }
+            else
+            {
+                $risultato->errore = "La query ha ritornato un risultato non valido";
+            }
         }
         $stmt->close(); 
         $conn->close(); 
@@ -448,9 +457,91 @@
         {
             return null;
         } 
+    }
+
+    function coordinateScuola($conn, $idUtente)
+    {
+        $risultato = new risultato();
+
+        $sql = "select via, n_civico, coordinate from scuole s inner join utenti u on u.cod_scuola = s.COD_meccanografico where u.ID_utente = ?"; 
+
+        $res = null; 
+
+        $stmt = $conn->prepare($sql); 
+
+        if($stmt === false)
+        {
+            $risultato->errore = "errore nella preparazione dello statement"; 
+            return $risultato; 
+        }
+        else
+        {
+            $stmt->bind_param("i", $idUtente); 
+            if(!$stmt->execute())
+            {
+                $risultato->errore = "errore nell'esecuzione dello statement"; 
+                return 
+            }
+            else
+            {
+                $res = $stmt->get_result(); 
+            }
+            if(controlloRisultato($res))
+            {
+                $risultato->result = $res; 
+            }
+            else
+            {
+                $risultato->errore = "La query ha ritornato un risultato non valido"; 
+            }
+
+            $stmt->close(); 
+            $conn->close(); 
+            return $risultato; 
+        }
+
+    }
+
+    //dato un utente ed un evento verifica se l'utente ha il permesso di effettuare modifiche ad esso
+    function verificaPermessi($idUtente, $idEvento, $conn){
+        $sql = "SELECT 1 AS res
+                FROM utenti u
+                JOIN eventi e ON e.ID_evento = ?
+                WHERE u.ID_utente = ?
+                AND (
+                    u.tipo = 'ADMIN'
+                    OR u.cod_scuola = e.cod_scuola
+                );";
+
+        if($stmt = $conn->prepare($sql)){
+            $stmt->bind_param("ii", $idEvento, $idUtente);
+
+
+            if($stmt->execute()) 
+            { 
+                $res = $stmt->get_result();
+                $risultato = $res->fetch_object(); 
+                var_dump($risultato);
+                $permesso = $risultato->res;
+                if($permesso=1){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            } 
+            else 
+            {
+                return null;
+            }
+        }
+        else{
+            return null;
+        }
 
 
     }
+
 
 
 
