6f50ee1e5f9d879cf450c29b0ad06e7f177eba51
edoardocori1312 <st10828@iismarconipieralisi.it>
2026-04-23 10:17:47 +0200

    Add files via upload

---
+++ Login_Gruppo_Accorroni_Beltrani_Campolucci_Cardinaletti_V4/datiConnessione.php
@@ -0,0 +1,16 @@
+<?php
+    // Nome dell'host del database.
+    // "localhost" significa che il database si trova sullo stesso server del sito
+    $HOSTDB="localhost";
+
+    // Nome dell'utente che si collega al database
+    // "root" è l'utente amministratore di MySQL
+    $USERDB="root";
+
+    // Password dell'utente del database
+    // In questo caso è impostata su "root"
+    $PASSDB="root";
+
+    // Nome del database che verrà utilizzato dall'applicazione
+    $NOMEDB="treelleorienta";
+?>
