2b9cc5f4f906f70ac18612acbefbf01ec0bcccc9
AndreaBatt2006 <st10523@iismarconipieralisi.it>
2026-05-07 12:44:39 +0200

    Add files via upload

---
+++ db.php
@@ -0,0 +1,9 @@
+# ProgettoAnno_3elleorienta_2025-2026
+
+
+Parte di amministrazione
+Ogni gruppo deve implementare la sua sezione di codice nella pagina apposta
+Tutte le pagine hanno la struttura di base necessaria a far funzionare il codice, non modificare altre pagine
+Se necessario aggiungere i propri file per il backend nella cartella pages/backend
+
+Il file di connessione al db è situato nel file config/db.php
