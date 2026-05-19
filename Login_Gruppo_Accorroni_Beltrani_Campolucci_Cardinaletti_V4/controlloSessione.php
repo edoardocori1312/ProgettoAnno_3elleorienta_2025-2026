978d8f9aab4ef4f57b5ba5cf404f5f0c6ff26e10
Angelo <st10934@iismarconipieralisi.it>
2026-05-19 10:03:35 +0200

    Delete Login_Gruppo_Accorroni_Beltrani_Campolucci_Cardinaletti_V4 directory

--- Login_Gruppo_Accorroni_Beltrani_Campolucci_Cardinaletti_V4/controlloSessione.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // Controlla se la variabile di sessione "emailUtente" NON esiste
-    if(!isset($_SESSION["emailUtente"]))
-    {
-        // Se non esiste significa che l'utente non ha effettuato il login
-        // quindi viene reindirizzato alla pagina di login
-        header("location:login.php");
-    }
-?>
--- Login_Gruppo_Accorroni_Beltrani_Campolucci_Cardinaletti_V4/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-
-    //cancellazione totale della sessione
-    session_destroy();
-    header('Location:login.php');
-    exit();
-
-?>
